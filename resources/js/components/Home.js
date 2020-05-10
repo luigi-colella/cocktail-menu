b5f82aef972e77c72cf257da4ee55354a54207bb
lgcolella <luigicolella.1997@gmail.com>
2020-05-11 01:19:12 +0200

    feature: added ingredient selection

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -3,13 +3,33 @@ import ReactDOM from 'react-dom';
 import IngredientList from './Home/IngredientList';
 
 class Home extends Component {
+    constructor (props) {
+        super(props)
+
+        this.state = {
+            selectedIngredient: null,
+        }
+
+        this.handleSelectIngredient = this.handleSelectIngredient.bind(this);
+    }
+
+    /**
+     * Save the ingredient selected by the user in the state.
+     * 
+     * @param {string} ingredient 
+     */
+    handleSelectIngredient (ingredient) {
+        this.setState({ selectedIngredient: ingredient })
+    }
 
     render () {
+        let { selectedIngredient } = this.state
+
         return (
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-4">
-                        <IngredientList />
+                        <IngredientList selectedIngredient={selectedIngredient} onSelectIngredient={this.handleSelectIngredient} />
                     </div>
                     <div className="col-md-4">
                         <div className="card">
--- resources/js/components/Home/IngredientList/IngredientsList.js
+++ resources/js/components/Home/IngredientList/IngredientsList.js
@@ -1,38 +1,55 @@
 import React, { Component } from 'react';
 import api from '../../../services/api';
+import Spinner from './Spinner';
 
 export default class IngredientList extends Component {
-
     constructor (props) {
         super(props);
+        
         this.state = {
-            ingredients: []
+            fetchingIngredients: false,
+            ingredientNames: [],
         };
     }
 
     componentDidMount () {
-        api.getIngredients().then((data) => {
-            console.log(data)
-            this.setState({ ingredients: data });
-        });
+        this.setState({fetchingIngredients: true});
+
+        api.getIngredients()
+            .then((data) => {
+                // map the response data into an array of names of ingredients and sort it
+                let ingredientNames = data.map(({name}) => name).sort();
+                this.setState({ ingredientNames: ingredientNames })
+            })
+            .finally(() => {
+                this.setState({ fetchingIngredients: false })
+            })
     }
 
     render () {
-        const ingredients = this.state.ingredients;
-        console.log(ingredients)
+        let { fetchingIngredients, ingredientNames } = this.state
+        let { selectedIngredient, onSelectIngredient } = this.props
 
         return (
             <div className="card">
-                <div className="card-header">Choose an ingredient!</div>
-
-                <div className="card-body">
-                    <table className="table table-hover">
-                        <tbody>
-                        {ingredients.map((ingredient) => {
-                            return <tr><td>{ingredient.name}</td></tr>
-                        })}
-                        </tbody>
-                    </table>
+                <div className="card-header">{selectedIngredient ? 'You selected: ' + selectedIngredient : 'Choose an ingredient!'}</div>
+                <div className="card-body" style={{ maxHeight: '75vh', overflow: 'auto' }}>
+                    {fetchingIngredients ? 
+                        <Spinner />
+                        :
+                        <table className="table table-hover">
+                            <tbody>
+                                {ingredientNames.map(name => {
+                                    let className = selectedIngredient === name ? 'bg-success text-white' : ''
+                                    return (
+                                        <tr key={name} onClick={() => { onSelectIngredient(name) }}>
+                                            <td className={'pointer-on-hover ' + className}>{name}</td>
+                                        </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    }
                 </div>
             </div>
         )
