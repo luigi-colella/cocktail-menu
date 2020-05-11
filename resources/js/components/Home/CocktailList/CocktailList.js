18c9dcc7a4022db98e245a5aaa206b7f8d7fc08d
lgcolella <luigicolella.1997@gmail.com>
2020-05-11 02:41:41 +0200

    refactor: add border and edited margin

--- resources/js/components/Home/CocktailList/CocktailList.js
+++ resources/js/components/Home/CocktailList/CocktailList.js
@@ -10,8 +10,8 @@ export default class CocktailList extends Component {
                     <ul className="list-unstyled">
                         {cocktails.map(cocktail => {
                             return (
-                                <li className="media align-items-center mt-3" key={cocktail.name}>
-                                    <img src={cocktail.thumbnailUrl} className="mr-3 w-25" style={{width: '20%'}} alt={cocktail.name} />
+                                <li className="media align-items-center mb-3" key={cocktail.name}>
+                                    <img src={cocktail.thumbnailUrl} className="mr-3 w-25 border border-dark" style={{width: '20%'}} alt={cocktail.name} />
                                     <div className="media-body">
                                         <h5 className="mt-0 mb-1">{cocktail.name}</h5>
                                     </div>
